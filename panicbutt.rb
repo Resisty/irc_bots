8d300d3914c187d07eaa14e12ae7943a16c2ad51
Brian Auron <brianauron@gmail.com>
2015-03-21 12:46:57 -0500

    Moved all panicbutt message parsing into panicbutt_helpers for best reloadability Added better(maybe?) help messages to panicbutt.

--- panicbutt.rb
+++ panicbutt.rb
@@ -31,75 +31,78 @@ bot = Cinch::Bot.new do
 
   on :message, /.*/ do |m|
     msg = m.message
-    msg, rep = parse_message(msg)
-    if msg
-      m.reply(msg, rep)
-    end
-  end
-
-  on :message, /^panicbutt tell (.+) to come to Portland/i do |m, who|
-    if who == "me"
-      m.reply("http://imgur.com/29hMr0h", true)
-    else
-      m.reply("@" + who + ": http://i.imgur.com/29hMr0h.jpg", false)
-    end
-  end
-
-  on :message, /^panicbutt roll dice($|.*)/i do |m, n|
-    if n == ''
-      m.reply(dice_roll('1').join(', '), true)
-    else
-      m.reply(dice_roll(n).join(', '), true)
-    end
-  end
-
-  on :message, /^panicbutt spin the wheel/i do |m, n|
-    m.reply(spin_wheel(), true)
-  end
-
-  on :message, /^panicbutt pants meat (.*)/i do |m, terms|
-    if terms == 'me'
-      m.reply('http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr06/2/18/anigif_enhanced-buzz-19264-1386027436-9.gif', true)
-    elsif terms.downcase == 'jeff'
-      def do_nothing
-        # let it happen further down /^panicbutt ((.*) Jeff|Jeff is at (.*))/
+    replies = parse_message(msg)
+    replies.each() do |message, rep|
+      if message
+        m.reply(message, rep)
       end
-    else
-      m.reply('@' + terms + ': http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr06/2/18/anigif_enhanced-buzz-19264-1386027436-9.gif', false)
     end
-  end
-
-  on :message, /^panicbutt butts ([\w-]+) (.*)/i do |m, user, terms|
-    if user == 'me'
-      m.reply(butts_me(terms), true)
-    else
-      m.reply('@' + user + ": " + butts_me(terms), false)
-    end
-  end
-
-  on :message, /^panicbutt (.*) Jeff$/i do |m, inner|
-    keys = $jeff_crisis_levels.keys()
-    if keys.any? {|k| k == inner }
-      m.reply(set_crisis_level(inner), true)
-    elsif ['list', 'enumerate', 'print'].any? {|word| word == inner}
-      m.reply(keys.join(", "), true)
-    end
-  end
-
-  on :message, /^panicbutt (|what is )*Jeff's existential crisis level/i do |m|
-    m.reply(crisis_level(), true)
   end
 
   on :message, /^panicbutt reload$/ do |m|
     m.reply(reload(), true)
   end
-  on :message, /^panicbutt testhelper method$/ do |m|
-    m.reply(testhelper(), true)
-  end
 
-  on :message, /^panicbutt Jeff's existential crisis level is (.*)/i do |m, term|
-    m.reply(set_crisis_level(term), true)
-  end
+#  on :message, /^panicbutt tell (.+) to come to Portland/i do |m, who|
+#    if who == "me"
+#      m.reply("http://imgur.com/29hMr0h", true)
+#    else
+#      m.reply("@" + who + ": http://i.imgur.com/29hMr0h.jpg", false)
+#    end
+#  end
+#
+#  on :message, /^panicbutt roll dice($|.*)/i do |m, n|
+#    if n == ''
+#      m.reply(dice_roll('1').join(', '), true)
+#    else
+#      m.reply(dice_roll(n).join(', '), true)
+#    end
+#  end
+#
+#  on :message, /^panicbutt spin the wheel/i do |m, n|
+#    m.reply(spin_wheel(), true)
+#  end
+#
+#  on :message, /^panicbutt pants meat (.*)/i do |m, terms|
+#    if terms == 'me'
+#      m.reply('http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr06/2/18/anigif_enhanced-buzz-19264-1386027436-9.gif', true)
+#    elsif terms.downcase == 'jeff'
+#      def do_nothing
+#        # let it happen further down /^panicbutt ((.*) Jeff|Jeff is at (.*))/
+#      end
+#    else
+#      m.reply('@' + terms + ': http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr06/2/18/anigif_enhanced-buzz-19264-1386027436-9.gif', false)
+#    end
+#  end
+#
+#  on :message, /^panicbutt butts ([\w-]+) (.*)/i do |m, user, terms|
+#    if user == 'me'
+#      m.reply(butts_me(terms), true)
+#    else
+#      m.reply('@' + user + ": " + butts_me(terms), false)
+#    end
+#  end
+#
+#  on :message, /^panicbutt (.*) Jeff$/i do |m, inner|
+#    keys = $jeff_crisis_levels.keys()
+#    if keys.any? {|k| k == inner }
+#      m.reply(set_crisis_level(inner), true)
+#    elsif ['list', 'enumerate', 'print'].any? {|word| word == inner}
+#      m.reply(keys.join(", "), true)
+#    end
+#  end
+#
+#  on :message, /^panicbutt (|what is )*Jeff's existential crisis level/i do |m|
+#    m.reply(crisis_level(), true)
+#  end
+#
+#  on :message, /^panicbutt testhelper method$/ do |m|
+#    m.reply(testhelper(), true)
+#  end
+#
+#  on :message, /^panicbutt Jeff's existential crisis level is (.*)/i do |m, term|
+#    m.reply(set_crisis_level(term), true)
+#  end
 end
 
 bot.start
--- panicbutt_helpers.rb
+++ panicbutt_helpers.rb
@@ -26,18 +26,53 @@ $jeff_crisis_levels = {'critical' => {'text' => 'black',
 $jeff_crisis_level = 'critical'
 
 def cmds()
- { '[A-Z]{3}' => { 'func' => :manatee_maybe, 'i' => false },
-   '^panicbutt(|,) enhance$' => { 'func' => :enhance, 'i' => true },
-   '^fuck (you|off)(|,) panicbutt$' => { 'func' => :fuck_off, 'i' => true },
-   '^panicbutt what is Jeff$' => { 'func' => :list_jeff, 'i' => true },
-   'cortana' => { 'func' => :cortana, 'i' => true },
-   '^panicbutt define windows error message (.*)$' => { 'func' => :windows_error, 'i' => true },
-   '^panicbutt sing [\w]+ happy birthday$' => { 'func' => :happy_birthday, 'i' => true },
-   '^panicbutt (-h|--help|help)$' => { 'func' => :panicbutt_help, 'i' => true },
+ { '[A-Z]{3}' => { 'func' => :manatee_maybe,
+                   'i' => false,
+                   'help' => 'Any block caps message returns a calming manatee to the agitated nick.'},
+   '^panicbutt(|,) enhance$' => { 'func' => :enhance,
+                                  'i' => true,
+                                  'help' => '"panicbutt, enhance" comma is optional. Panicbutt will enhance the image.'},
+   '^fuck (you|off)(|,) panicbutt$' => { 'func' => :fuck_off,
+                                         'i' => true,
+                                         'help' => '"fuck you/off, panicbutt" comma is optional. Panicbutt is just trying to help, jeez'},
+   '^panicbutt what is Jeff$' => { 'func' => :list_jeff,
+                                   'i' => true,
+                                   'help' => '"panicbutt what is Jeff" will tell you Jeff\'s current existential crisis level. Defaults to critical'},
+   'cortana' => { 'func' => :cortana,
+                  'i' => true,
+                  'help' => 'Any mention of cortana will summon the AI who helped to save the galaxy.'},
+   '^panicbutt define windows error message (.*)$' => { 'func' => :windows_error,
+                                                        'i' => true,
+                                                        'help' => '"panicbutt define windows error message <error string>" panicbutt will try to sort through the incomprehensible mess which is Windows and report back on (hopefully) useful solutions.'},
+   '^panicbutt sing [\w]+ happy birthday$' => { 'func' => :happy_birthday,
+                                                'i' => true,
+                                                'help' => '"panicbutt sing <somebody> happy birthday" panicbutt will serenade somebody with the joyful, celebratory renditions of MC Chris'},
+   '^panicbutt tell (.+) to come to Portland$' => { 'func' => :come_to_portland,
+                                                    'i' => true,
+                                                    'help' => '"panicbutt tell <somebody> to come to Portland" panicbutt will summon the raw, animal magnetism of Hugh Jackman in an attempt to convince somebody to come to Portland. Shhh, just come.'},
+   '^panicbutt roll dice($|.*)' => { 'func' => :roll_dice,
+                                     'i' => true,
+                                     'help' => '"panicbutt roll dice [number of 6 sided dice]" OR "panicbutt roll dice [set of dice, e.g. 2d8 3d12 1d20] [with <something> modifier +/-<number>]" Panicbutt will try to figure out what the fuck kind of dice you want to roll and roll them. Including the optional modifier string will adjust dice accordingly.'},
+   '^panicbutt spin the wheel' => { 'func' => :spin_wheel,
+                                    'i' => true,
+                                    'help' => '"panicbutt spin the wheel" makes panicbutt spin the Price is Right wheel, not the Wheel of Fortune Wheel \'cuz that\'d be dumb'},
+   '^panicbutt butts ([\w-]+) (.*)' => { 'func' => :butts_me,
+                                         'i' => true,
+                                         'help' => '"panicbutt butts <somebody> <something>" panicbutt will look up <something> (can contain spaces) on giphy for <somebody> (cannot contain spaces, can be "me")'},
+   '^panicbutt (.*) Jeff$' => { 'func' => :jeff_stuff,
+                                'i' => true,
+                                'help' => '"panicbutt <something> Jeff" Does stuff with Jeff\'s existential crisis levels. Options [list,enumerate,print] provide what settings are available for the crisis levels. Options [url,link] provide the web url to view the crisis level in action. Using any of the crisis levels from "list" sets the crisis level accordingly.'},
+   '^panicbutt can bobi spend this money' => { 'func' => :can_bobi_spend,
+                                               'i' => true,
+                                               'help' => '"panicbutt can bobi spend this money" provides a webpage answering your question'},
+   '^panicbutt (-h|--help|help)$' => { 'func' => :panicbutt_help,
+                                       'i' => true,
+                                       'help' => '"panicbutt -h, panicbutt --help, panicbutt help" print this list of helpful help messages.'},
  }
 end
 
 def parse_message(msg)
+  replies = []
   cmds.keys().each() do |k|
     if cmds[k]['i']
       reg = Regexp.new(k, Regexp::IGNORECASE)
@@ -45,30 +80,34 @@ def parse_message(msg)
       reg = Regexp.new k
     end
     if msg =~ reg
-      return send(cmds[k]['func'], msg)
+      replies.push(send(cmds[k]['func'], msg, reg))
     end
   end
-  return nil, nil
+  if replies
+    return replies
+  else
+    return [[nil, nil]]
+  end
 end
 
-def enhance(msg)
+def enhance(msg, reg)
   return "/me types furiously. \"Enhance.\"", false
 end
 
-def list_jeff(msg)
+def list_jeff(msg, reg)
   return $jeff_crisis_level, true
 end
 
-def fuck_off(msg)
+def fuck_off(msg, reg)
   return ":C", true
 end
 
-def cortana(msg)
+def cortana(msg, reg)
   return 'http://i0.kym-cdn.com/photos/images/original/000/837/637/7d6.gif', true
 end
 
-def windows_error(msg)
-  code = msg.scan(/message (.*)/)
+def windows_error(msg, reg)
+  code = msg.scan(reg)
   roll = rand(1000)
   if roll < 150
     return 'Suggested course of action for code %s: install your choice of linux distro' % code[0][0] , true
@@ -79,18 +118,53 @@ def windows_error(msg)
   end
 end
 
-def happy_birthday(msg)
-  nick = msg.scan(/sing ([\w]+) happy/)
+def happy_birthday(msg, reg)
+  nick = msg.scan(reg)
   nick = nick[0][0]
   return "Go #{nick}, it\'s your birthday! Go #{nick}, it\'s your birthday! You're one year older, one year wiser, you're a rock 'n roll star, king, czar and kaiser! You're the man of the hour, the VIP! You get the first slice -- of the P-I-E! So blow out your candles and make a wish! Put a smile on -- 'cuz it's your birthday, bitch! Go #{nick}, it's your birthday! Go #{nick}, it's your birthday!", false
 end
 
-def panicbutt_help(msg)
-  msg = "Panicbutt understands the following regexes:\n"
-  cmds.keys().each() do |k|
-    msg += "#{k}\n"
+def come_to_portland(msg, reg)
+  who = msg.scan(reg)
+  if who[0][0] == 'me'
+    return "http://imgur.com/29hMr0h", true
+  else
+     return who[0][0] + ": http://i.imgur.com/29hMr0h.jpg", false
   end
-  msg += "Panicbutt also understands more but BOBI was too lazy to put them in just now."
+end
+
+def roll_dice(msg, reg)
+  n = msg.scan(reg)
+  if n[0][0] == ''
+    return dice_roll('1').join(', '), true
+  else
+    return dice_roll(n[0][0]).join(', '), true
+  end
+end
+
+def jeff_stuff(msg, reg)
+  verb = msg.scan(reg)
+  verb = verb[0][0]
+  keys = $jeff_crisis_levels.keys()
+  if keys.any? {|k| k == verb }
+    return set_crisis_level(verb), true
+  elsif ['list', 'enumerate', 'print'].any? {|word| word == verb}
+    return keys.join(", "), true
+  elsif ['link', 'url'].any? {|word| word == verb}
+    return "http://brianauron.info/jeff-existential-crisis-level/", true
+  end
+end
+
+def can_bobi_spend(msg, reg)
+  return "http://brianauron.info/CanBobiSpendThisMoney", true
+end
+
+def panicbutt_help(msg, reg)
+  msg = "Panicbutt understands the following messages:\n"
+  cmds.keys().each() do |k|
+    cmd = cmds[k]['help']
+    msg += "#{cmd}\n"
+  end
   return msg, true
 end
 
@@ -150,7 +224,7 @@ def set_crisis_level(level)
   return text
 end
 
-def manatee_maybe(msg)
+def manatee_maybe(msg, reg)
   if msg == msg.upcase && msg.length > 4
     return manatee(), true
   end
@@ -163,9 +237,19 @@ def manatee()
 end
 
 
-def butts_me(search)
+def butts_me(msg, reg)
+  terms = msg.scan(reg)
+  nick = terms[0][0]
+  if nick == 'me'
+    rep = true
+  else
+    rep = false
+  end
+  search = terms[0][1]
   if search == 'manatee'
-    return manatee()
+    return manatee(), rep
+  elsif search == 'pants meat'
+    return 'http://s3-ec.buzzfed.com/static/2013-12/enhanced/webdr06/2/18/anigif_enhanced-buzz-19264-1386027436-9.gif', rep
   end
   search = search.split(/ /).join('+')
   url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + search
@@ -174,9 +258,9 @@ def butts_me(search)
   resp = Net::HTTP::get_response(uri)
   result = JSON.parse(resp.body)['data']
   if result.empty?
-    return 'That\'s a stupid search!' 
+    return 'That\'s a stupid search!', rep 
   else
-    return result['image_original_url']
+    return result['image_original_url'], rep
   end
 end
 
@@ -217,7 +301,7 @@ def dice_roll(dicestr)
 end
 
 
-def spin_wheel()
+def spin_wheel(msg, reg)
   values = (5..100).step(5)
-  return values.to_a.sample
+  return values.to_a.sample, true
 end
